f271b3e11aab18a8353400864bff3bc236bed5bc
Enver Selchuk <Enver.Selchuk@gamma.co.uk>
2020-07-15 13:21:09 +0100

    Couese with table view removed Course.tsx

--- src/webparts/reactDemo/components/Courses.tsx
+++ src/webparts/reactDemo/components/Courses.tsx
@@ -3,7 +3,6 @@ import styles from './ReactDemo.module.scss';
 
 import { CourseProvider } from "../../../services/CourseProvider";
 import { ICourse } from "../../../common/ICourse";
-import Course from "./Course";
 
 export interface ICoursesProps {
   context: any;
@@ -47,20 +46,44 @@ export default class Courses extends React.Component<ICoursesProps, ICoursesStat
         <div className={styles.container}>
           <span className={styles.title}>Courses</span>
           <p className={styles.subTitle}>List of Courses</p>
-          <input type="button" value="Delete" onClick={() => {
+          <input type="button" value=" Delete " onClick={() => {
             let items: ICourse[] = [...this.state.data];
+
             items.slice();
+
             this.setState({
               data: items
             });
+
           }} />
           <div className={styles.row}>
-            {
-              this.state.data.map((c: ICourse) => <Course item={c} />)
-            }
+            <div className={styles.column}>
+              <table>
+                <tr>
+                  <td>ID</td>
+                  <td>Name</td>
+                  <td>Category</td>
+                  <td>Hrs</td>
+                  <td>Price</td>
+                </tr>
+                {
+                  this.state.data.map((c: ICourse) => this.getCourseRow(c))
+                }
+              </table>
+            </div>
           </div>
         </div>
       </div>
     );
+  }
+
+  private getCourseRow(c: ICourse) {
+    return <tr>
+      <td>{c.CourseID}</td>
+      <td>{c.Title}</td>
+      <td>{c.Category} </td>
+      <td>{c.Duration} Hrs</td>
+      <td>$ {c.Price}</td>
+    </tr>;
   }
 }
