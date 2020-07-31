a34dde8919c877d95609c8d1fdbb4a2d9994a9c4
Enver Selchuk <Enver.Selchuk@gamma.co.uk>
2020-07-31 12:46:59 +0100

    FINAL CODE FOR DATE PICKDER + 90 or -90 days range

--- src/webparts/fluentUiDemo/components/PickerDemo.tsx
+++ src/webparts/fluentUiDemo/components/PickerDemo.tsx
@@ -2,34 +2,78 @@ import * as React from 'react';
 import styles from './FluentUiDemo.module.scss';
 import { IFluentUiDemoProps } from './IFluentUiDemoProps';
 
-import { Fabric, Label, DatePicker, DayOfWeek } from "office-ui-fabric-react";
+import { Fabric, Label, DatePicker, DayOfWeek, mergeStyleSets } from "office-ui-fabric-react";
 
 
 interface IPickerDemoState {
-    selDate: Date;
+  selDate: Date;
 }
 
 interface IPickerDemoProps {
-    context?: any;
+  context?: any;
+}
+
+const datepickerStyle = mergeStyleSets({
+  control: {
+    maxWidth: '250px'
+  }
+});
+
+const addDays = (date: Date, d: number): Date => {
+  let newDate = new Date(date.getTime());
+
+  newDate.setDate(newDate.getDate() + d);
+
+  return newDate;
 }
 
 export default class PickerDemo extends React.Component<IPickerDemoProps, IPickerDemoState> {
+  private minDate: Date;
+  private maxDate: Date;
+
   constructor(props: IPickerDemoProps) {
     super(props);
+    let d = new Date();
+
+    this.minDate = addDays(d, -90);
+    this.maxDate = addDays(d, 90);
+
+    console.log(`Min:${this.minDate.toDateString()}, Max Date: ${this.maxDate.toDateString()}`);
 
     this.state = {
-        selDate: new Date()
+      selDate: new Date()
     };
+  }
 
+  private formateDate(value: Date): string {
+    return value.getDate() + "/" + (value.getMonth() + 1) + "/" +
+      value.getFullYear();
   }
 
   public render(): React.ReactElement<IFluentUiDemoProps> {
     return (
-        <Fabric>
-              <div>
-                <DatePicker label="Start Date: " placeholder="Pick a Date" />
-              </div>
-        </Fabric>
+      <Fabric>
+        <div>
+          <DatePicker label="Start Date: "
+            firstDayOfWeek={DayOfWeek.Monday}
+            placeholder="Pick a Date"
+            className={datepickerStyle.control}
+            onSelectDate={(value: Date) => {
+              this.setState({
+                selDate: value
+              });
+            }}
+
+            formatDate={this.formateDate}
+            minDate={this.minDate}
+            maxDate={this.maxDate}
+          />
+        </div>
+        <div>
+          <Label>DATA</Label>
+          <Label>Date: {this.state.selDate.toDateString()} </Label>
+        </div>
+      </Fabric>
     );
   }
 }
