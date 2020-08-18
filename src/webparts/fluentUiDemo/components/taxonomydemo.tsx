32824a9b4a46bfbee2d22871c3312c69b5d569f0
Enver Selchuk <Enver.Selchuk@gamma.co.uk>
2020-08-18 12:19:04 +0100

    With initial values set already for this taxonomy

--- src/webparts/fluentUiDemo/components/taxonomydemo.tsx
+++ src/webparts/fluentUiDemo/components/taxonomydemo.tsx
@@ -5,6 +5,20 @@ import { Label } from "office-ui-fabric-react";
 
 import { TaxonomyPicker, IPickerTerms } from "@pnp/spfx-controls-react/lib/TaxonomyPicker";
 
+const initVal : IPickerTerms = [
+    {
+      "name": "Cloud",
+      "key": "53ab5ed9-990d-46d0-9c2e-58fbe4162ec9", // change this depend on envirnment
+      "path": "Cloud",
+      "termSet": "d3964f36-6120-486e-921c-07adb73357b9" // change this depend on envirnment
+    },
+    {
+      "name": "DevOps",
+      "key": "de8d81ac-2284-4979-bc94-89f16ce00fa0", // change this depend on envirnment
+      "path": "DevOps",
+      "termSet": "d3964f36-6120-486e-921c-07adb73357b9" // change this depend on envirnment
+    }];
+
 export default class TaxonomyDemo extends React.Component<any, any> {
 
     constructor(props: any) {
@@ -18,7 +32,9 @@ export default class TaxonomyDemo extends React.Component<any, any> {
     public render(): JSX.Element {
         return (
             <div>
-                <TaxonomyPicker context={ this.props.context } label="Pick a Term:"
+                <TaxonomyPicker context={ this.props.context } 
+                    initialValues={ initVal }
+                    label="Pick a Term:"
                     panelTitle="Select Term"
                     termsetNameOrID="Categories"
                     isTermSetSelectable={ false }
@@ -26,6 +42,8 @@ export default class TaxonomyDemo extends React.Component<any, any> {
                     placeholder="Select Category"
                     onChange={ (terms: IPickerTerms) => {
                         let selTerms: string[] = [];
+
+                        console.log("Terms: " + JSON.stringify(terms));
 
                         for(let t of terms) {
                             selTerms.push(t.name);
