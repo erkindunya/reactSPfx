4d2962ac5d2561516f3b6d2e31b0e2c15d094720
Enver Selchuk <Enver.Selchuk@gamma.co.uk>
2020-08-18 12:01:38 +0100

    ERROR: Cannot read property 'selectedTerms' of null

--- src/webparts/fluentUiDemo/components/taxonomydemo.tsx
+++ src/webparts/fluentUiDemo/components/taxonomydemo.tsx
@@ -1,21 +1,44 @@
 import * as React from 'react';
 import styles from './FluentUiDemo.module.scss';
 
-import { TaxonomyPicker } from "@pnp/spfx-controls-react/lib/TaxonomyPicker";
+import { Label } from "office-ui-fabric-react";
+
+import { TaxonomyPicker, IPickerTerms } from "@pnp/spfx-controls-react/lib/TaxonomyPicker";
 
 export default class TaxonomyDemo extends React.Component<any, any> {
 
     constructor(props: any) {
         super(props);
+
+        this.setState({
+            selectedTerms: []
+        });
     }
 
     public render(): JSX.Element {
         return (
-            <TaxonomyPicker context={ this.props.context } label="Pick a Term:"
-                panelTitle="Select Term"
-                termsetNameOrID="Categories"
-                allowMultipleSelections={ false }
-            />
+            <div>
+                <TaxonomyPicker context={ this.props.context } label="Pick a Term:"
+                    panelTitle="Select Term"
+                    termsetNameOrID="Categories"
+                    allowMultipleSelections={ true }
+                    onChange={ (terms: IPickerTerms) => {
+                        let selTerms: string[] = [];
+
+                        for(let t of terms) {
+                            selTerms.push(t.name);
+                        }
+
+                        this.setState({
+                            selectedTerms: selTerms
+                        });
+                    }}
+                />
+                <Label>Selected Term Names : </Label><br/>
+                {
+                    this.state.selectedTerms.map(t => <Label>{ t }</Label>)
+                }
+            </div>
         );
     }
 }
